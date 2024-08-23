7774e7b0a35b6f16003cb336911d40ab961af277
Sura <suri.murodova@gmail.com>
2024-08-23 11:21:43 +0300

    Add storage module.
    Add storage module with FilesSaver class to save bulletin files.

---
+++ Data_Bases/bulletin_db/files_handlers/storage.py
@@ -0,0 +1,14 @@
+import os
+
+
+class FilesSaver:
+    def __init__(self, xls_files_dict: dict):
+        self.xls_files_dict = xls_files_dict
+        self.files_name = []
+
+    def save_files(self):
+        for file_name, content in self.xls_files_dict.items():
+            self.files_name.append(file_name)
+            dir_path = os.path.join('bulletins', file_name)
+            with open(dir_path, 'wb') as file:
+                file.write(content)
