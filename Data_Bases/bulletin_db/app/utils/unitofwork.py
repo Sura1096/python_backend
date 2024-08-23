501328037581503ad6fde8226f61fc1b96be0499
Sura <suri.murodova@gmail.com>
2024-08-23 18:28:20 +0300

    Add UoW for trading repo

---
+++ Data_Bases/bulletin_db/app/utils/unitofwork.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+
+from db.database import async_session_maker
+from repositories.trading_repository import TradingRepository
+
+
+class IUnitOfWork(ABC):
+    trading: TradingRepository
+
+    @abstractmethod
+    def __init__(self): ...
+
+    @abstractmethod
+    async def __aenter__(self): ...
+
+    @abstractmethod
+    async def __aexit__(self, *args): ...
+
+    @abstractmethod
+    async def commit(self): ...
+
+    @abstractmethod
+    async def rollback(self): ...
+
+
+class UnitOfWork(IUnitOfWork):
+    def __init__(self):
+        self.session_factory = async_session_maker
+
+    async def __aenter__(self):
+        self.session = self.session_factory()
+        self.trading = TradingRepository(self.session)
+        return self
+
+    async def __aexit__(self, *args):
+        await self.rollback()
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
