f64b503dab9926c1876b24f248269510b076679a
Sura <suri.murodova@gmail.com>
2024-08-23 11:54:46 +0300

    Add SQLAlchemy model

---
+++ Data_Bases/bulletin_db/db/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Integer, Date, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from database import Base
+
+
+class SpimexTradingResults(Base):
+    __tablename__ = 'spimex_trading_results'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    exchange_product_id: Mapped[str]
+    exchange_product_name: Mapped[str]
+    oil_id: Mapped[str]
+    delivery_basis_id: Mapped[str]
+    delivery_basis_name: Mapped[str]
+    delivery_type_id: Mapped[str]
+    volume: Mapped[int]
+    total: Mapped[int]
+    count: Mapped[int]
+    date: Mapped[Date]
+    created_on: Mapped[Date] = mapped_column(Date, server_default=func.current_date())
+    updated_on: Mapped[Date] = mapped_column(Date, onupdate=func.current_date())
