d32bf2fecf20cdbbd7f35cdb50c5e6f2e3369539
Sura <suri.murodova@gmail.com>
2024-08-23 18:43:19 +0300

    Place the url generator in a static method.

--- Data_Bases/bulletin_db/app/external_services/parser.py
+++ Data_Bases/bulletin_db/app/external_services/parser.py
@@ -6,17 +6,26 @@ class WebParser:
     def __init__(self):
         self.url = 'https://spimex.com/upload/reports/oil_xls/oil_xls_202312'
 
-    def _get_all_url(self) -> dict:
-        xls_files = {}
+    @staticmethod
+    def __generate_urls(start_url):
+        urls = []
         for date in range(1, 32):
             full_url = (
-                f'{self.url}0{date}162000.xls'
+                f'{start_url}0{date}162000.xls'
                 if len(str(date)) < 2
-                else f'{self.url}{date}162000.xls'
+                else f'{start_url}{date}162000.xls'
             )
+            urls.append(full_url)
+        return urls
+
+    def _get_all_url(self) -> dict:
+        urls = self.__generate_urls(self.url)
+        xls_files = {}
+
+        for url in urls:
             try:
-                url = urlopen(full_url)
-                file_name = full_url.split('/')[-1]
+                url = urlopen(url)
+                file_name = url.split('/')[-1]
                 xls_files[file_name] = url.read()
             except urllib.error.HTTPError:
                 continue
