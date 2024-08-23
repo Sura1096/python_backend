8b1cc1e82f7bb4b6aac72b13e431f0b40c42ee73
Sura <suri.murodova@gmail.com>
2024-08-23 18:51:57 +0300

    Add docstring for methods

--- Data_Bases/bulletin_db/app/external_services/parser.py
+++ Data_Bases/bulletin_db/app/external_services/parser.py
@@ -4,10 +4,22 @@ import urllib.error
 
 class WebParser:
     def __init__(self):
+        """
+        Инициализирует объект WebParser с начальным URL.
+        """
         self.url = 'https://spimex.com/upload/reports/oil_xls/oil_xls_202312'
 
     @staticmethod
     def __generate_urls(start_url):
+        """
+        Генерирует список URL для загрузки XLS-файлов с сайта spimex.com.
+
+        Args:
+            start_url (str): Начальный URL для генерации.
+
+        Returns:
+            list: Список сгенерированных URL.
+        """
         urls = []
         for date in range(1, 32):
             full_url = (
@@ -19,6 +31,12 @@ class WebParser:
         return urls
 
     def _get_all_url(self) -> dict:
+        """
+        Возвращает словарь валидных URL для скачивания XLS-файлов с сайта spimex.com.
+
+        Returns:
+            dict: Словарь, где ключ - имя файла, а значение - содержимое файла.
+        """
         urls = self.__generate_urls(self.url)
         xls_files = {}
 
