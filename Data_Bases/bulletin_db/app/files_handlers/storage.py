8fa2aa7fab964319e744dc2c9f27988af6667007
Sura <suri.murodova@gmail.com>
2024-08-23 19:18:10 +0300

    Add doctring and type hints for methods

--- Data_Bases/bulletin_db/app/files_handlers/storage.py
+++ Data_Bases/bulletin_db/app/files_handlers/storage.py
@@ -3,10 +3,22 @@ import os
 
 class FilesSaver:
     def __init__(self, xls_files_dict: dict):
+        """
+        Класс для сохранения XLS-файлов.
+        """
         self.xls_files_dict = xls_files_dict
         self.files_name = []
 
-    def save_files(self):
+    def save_files(self) -> None:
+        """
+        Сохраняет XLS-файлы в папку "bulletins".
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         for file_name, content in self.xls_files_dict.items():
             self.files_name.append(file_name)
             dir_path = os.path.join('bulletins', file_name)
