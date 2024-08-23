85fc73ed8093b070356e8422638538c78d20fb89
Sura <suri.murodova@gmail.com>
2024-08-23 11:54:22 +0300

    Add db setup

---
+++ Data_Bases/bulletin_db/db/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+from ..core.config import DATABASE_URL
+
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
+
+
+class Base(DeclarativeBase):
+    pass
