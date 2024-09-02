1197714785d89a7636b7bc919b33c03f305eb3e6
Sura <suri.murodova@gmail.com>
2024-09-02 16:58:50 +0300

    Add class-based API view controllers for breeds

--- Django/django_dog_api/app/breed_api/views.py
+++ Django/django_dog_api/app/breed_api/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from breed_api.models import Breed
+from breed_api.serializers import BreedSerializer
+
+
+class BreedList(APIView):
+    """List all breeds, or create a new breed."""
+
+    def get(self, request: Request):
+        dog_queryset = Breed.objects.all()
+        serializer = BreedSerializer(dog_queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request: Request):
+        serializer = BreedSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BreedDetail(APIView):
+    """Retrieve, update or delete a breed instance."""
+
+    def __get_object(self, pk: int):
+        try:
+            return Breed.objects.get(pk=pk)
+        except Breed.DoesNotExist:
+            raise Http404
+
+    def get(self, request: Request, pk: int):
+        dog_obj = self.__get_object(pk)
+        serializer = BreedSerializer(dog_obj)
+        return Response(serializer.data)
+
+    def put(self, request: Request, pk: int):
+        dog_obj = self.__get_object(pk)
+        serializer = BreedSerializer(dog_obj, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, pk: int):
+        dog_obj = self.__get_object(pk)
+        dog_obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
