d7ada25113f1223b4bd66936ae31806fe26df7c9
Sura <suri.murodova@gmail.com>
2024-09-02 15:13:27 +0300

    Create Breed model

--- Django/django_dog_api/app/breed_api/models.py
+++ Django/django_dog_api/app/breed_api/models.py
@@ -1,3 +1,30 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Breed(models.Model):
+    name = models.CharField(max_length=255)
+    size = models.CharField(
+        max_length=10,
+        choices=[
+            ('Tiny', 'Tiny'),
+            ('Small', 'Small'),
+            ('Medium', 'Medium'),
+            ('Large', 'Large'),
+        ],
+    )
+    friendliness = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+    )
+    trainability = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+    )
+    shedding_amount = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+    )
+    exercise_needs = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+    )
+
+    def __str__(self) -> models.CharField:
+        return self.name
