962a9155d857c505daae6d9025168d7ad1a021ce
Sura <suri.murodova@gmail.com>
2024-09-02 15:16:56 +0300

    Delete Breed model

--- Django/django_dog_api/app/dog_api/models.py
+++ Django/django_dog_api/app/dog_api/models.py
@@ -1,33 +1,5 @@
-from django.core.validators import MaxValueValidator, MinValueValidator
+from breed_api.models import Breed
 from django.db import models
-
-
-class Breed(models.Model):
-    name = models.CharField(max_length=255)
-    size = models.CharField(
-        max_length=10,
-        choices=[
-            ('Tiny', 'Tiny'),
-            ('Small', 'Small'),
-            ('Medium', 'Medium'),
-            ('Large', 'Large'),
-        ],
-    )
-    friendliness = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(5)],
-    )
-    trainability = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(5)],
-    )
-    shedding_amount = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(5)],
-    )
-    exercise_needs = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(5)],
-    )
-
-    def __str__(self) -> models.CharField:
-        return self.name
 
 
 class Dog(models.Model):
