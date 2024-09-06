807155afb7b7524c4d07a6131aac31fa8cc36d84
Sura <suri.murodova@gmail.com>
2024-09-06 15:03:01 +0300

    Fix breed serializer

--- Django/django_dog_api/app/breed_api/serializers.py
+++ Django/django_dog_api/app/breed_api/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from breed_api.models import Breed
@@ -7,3 +8,27 @@ class BreedSerializer(ModelSerializer):
     class Meta:
         model = Breed
         fields = '__all__'
+
+    def validate_friendliness(self, value: int) -> int:
+        if not 1 <= value <= 5:
+            msg = 'Friendliness must be between 1 and 5.'
+            raise serializers.ValidationError(msg)
+        return value
+
+    def validate_trainability(self, value: int) -> int:
+        if not 1 <= value <= 5:
+            msg = 'Trainability must be between 1 and 5.'
+            raise serializers.ValidationError(msg)
+        return value
+
+    def validate_shedding_amount(self, value: int) -> int:
+        if not 1 <= value <= 5:
+            msg = 'Shedding amount must be between 1 and 5.'
+            raise serializers.ValidationError(msg)
+        return value
+
+    def validate_exercise_needs(self, value: int) -> int:
+        if not 1 <= value <= 5:
+            msg = 'Exercise needs must be between 1 and 5.'
+            raise serializers.ValidationError(msg)
+        return value
