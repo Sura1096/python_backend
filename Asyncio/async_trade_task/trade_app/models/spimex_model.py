0d1cb0f5e60d43a42c4bc8561240f5acbc6fa9a0
Sura <suri.murodova@gmail.com>
2024-09-11 15:45:05 +0300

    Add a package for SQLAlchemy models

---
+++ Asyncio/async_trade_task/trade_app/models/spimex_model.py
@@ -0,0 +1,24 @@
+from datetime import date
+
+from sqlalchemy import BigInteger, Date, Integer, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class SpimexTradingResults(Base):
+    __tablename__ = 'spimex_trading_results'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    exchange_product_id: Mapped[str]
+    exchange_product_name: Mapped[str]
+    oil_id: Mapped[str]
+    delivery_basis_id: Mapped[str]
+    delivery_basis_name: Mapped[str]
+    delivery_type_id: Mapped[str]
+    volume: Mapped[int]
+    total: Mapped[int] = mapped_column(BigInteger)
+    count: Mapped[int]
+    date: Mapped[date]
+    created_on: Mapped[date] = mapped_column(Date, server_default=func.current_date())
+    updated_on: Mapped[date] = mapped_column(Date, onupdate=func.current_date())
