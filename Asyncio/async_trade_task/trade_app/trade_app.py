75a46ffeecee38e335a3932daf0f111a114789de
Sura <suri.murodova@gmail.com>
2024-09-11 15:50:01 +0300

    Add the main package to run trade app

---
+++ Asyncio/async_trade_task/trade_app/trade_app.py
@@ -0,0 +1,38 @@
+import asyncio
+import time
+
+from external_services.parser import WebParser
+from files_handlers.file_content import files_parse
+from files_handlers.storage import FilesSaver
+from services.service import TradeService
+from utils.unit_of_work import UnitOfWork
+
+
+async def save_bulletin_files() -> None:
+    parse_urls = WebParser()
+    url_dict = await parse_urls.get_files()
+
+    files_saver = FilesSaver(**url_dict)
+    await files_saver.save_files()
+
+
+async def save_into_db() -> None:
+    bulletins_info = await files_parse()
+    async with UnitOfWork() as uow:
+        trading_service = TradeService(uow)
+        for content in bulletins_info.values():
+            for cell in content:
+                await trading_service.add_new_trade(**cell)
+
+
+async def main() -> None:
+    save_files_task = asyncio.create_task(save_bulletin_files())
+    save_info_into_db_task = asyncio.create_task(save_into_db())
+    await asyncio.gather(save_files_task, save_info_into_db_task)
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    asyncio.run(main())
+    end_time = time.time()
+    print(f'Время выполнения асинхронного кода: {end_time - start_time:.2f} секунд.')
