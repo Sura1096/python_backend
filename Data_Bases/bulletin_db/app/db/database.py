dcc20a0fa1d137ab735b03b1a897e88b14c6dab4
Sura <suri.murodova@gmail.com>
2024-09-11 16:04:59 +0300

    Fix imports

--- Data_Bases/bulletin_db/app/db/database.py
+++ Data_Bases/bulletin_db/app/db/database.py
@@ -1,8 +1,6 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.orm import DeclarativeBase
-
 from core.config import DATABASE_URL
-
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
