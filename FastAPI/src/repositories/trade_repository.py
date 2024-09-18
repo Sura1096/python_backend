ba79bbeb9f5a016473b66531845fee737a9a00a6
Sura <suri.murodova@gmail.com>
2024-09-18 17:55:35 +0300

    Add docstring

--- FastAPI/src/repositories/trade_repository.py
+++ FastAPI/src/repositories/trade_repository.py
@@ -15,6 +15,11 @@ class TradeRepository(SqlAlchemyRepository):
     model = SpimexTradingResults
 
     async def get_last_trading_dates(self, lim: int) -> Sequence[LastTradeResponse]:
+        """Возвращает список дат последних торговых дней.
+
+        :param lim: Количество последних торговых дат, которые нужно вернуть.
+        :return: Список объектов, представляющих последние торговые даты.
+        """
         query = (
             select(distinct(self.model.date))
             .order_by(self.model.date.desc())
@@ -27,6 +32,17 @@ class TradeRepository(SqlAlchemyRepository):
         self,
         trade_filters: TradeDynamicsRequest,
     ) -> Sequence[SpimexTradingResults]:
+        """Возвращает список торгов за заданный период.
+
+        :param trade_filters: Объект с фильтрами для запроса данных:
+            - start_date (Начальная дата). Обязательный параметр, чтобы определить, от какой даты начинать выборку;
+            - end_date (Конечная дата). Обязательный параметр для установки границы выборки;
+            - oil_id. Необязательный параметр;
+            - delivery_type_id. Необязательный параметр;
+            - delivery_basis_id. Необязательный параметр.
+
+        :return: Список объектов, представляющих результаты торговли в заданном диапазоне.
+        """
         query = select(self.model).filter(
             self.model.date >= trade_filters.start_date,
             self.model.date <= trade_filters.end_date,
@@ -49,6 +65,15 @@ class TradeRepository(SqlAlchemyRepository):
         self,
         trade_filters: TradeResultsRequest,
     ) -> Sequence[SpimexTradingResults]:
+        """Возвращает список последних торгов.
+
+        :param trade_filters: Объект с фильтрами для запроса данных:
+            - oil_id. Необязательный параметр;
+            - delivery_type_id. Необязательный параметр;
+            - delivery_basis_id. Необязательный параметр.
+
+        :return: Список объектов, представляющих результаты торговли, удовлетворяющие фильтрам.
+        """
         query = select(self.model)
         if trade_filters.oil_id:
             query = query.filter(self.model.oil_id == trade_filters.oil_id)
