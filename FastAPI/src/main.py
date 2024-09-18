c1e80d63cc0ed6aa3ee5a59b5acfc3c64d5d6d87
Sura <suri.murodova@gmail.com>
2024-09-18 17:09:59 +0300

    Rewrite redis url using config module

--- FastAPI/src/main.py
+++ FastAPI/src/main.py
@@ -7,11 +7,12 @@ from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
 
 from src.api.routers.trade import router
+from src.core.config import redis_config
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncIterator[None]:
-    redis = aioredis.from_url('redis://localhost')
+    redis = aioredis.from_url(redis_config.REDIS_URL)
     FastAPICache.init(RedisBackend(redis), prefix='fastapi-cache')
     yield
 
