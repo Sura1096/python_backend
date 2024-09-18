aa1ce0cc46eb03e69097195fd2807ea5404875ba
Sura <suri.murodova@gmail.com>
2024-09-18 14:57:43 +0300

    Add query caching

--- FastAPI/src/api/routers/trade.py
+++ FastAPI/src/api/routers/trade.py
@@ -1,13 +1,15 @@
-from fastapi import APIRouter, Depends
+from datetime import datetime, timedelta
 
-from src.api.services.trade_service import TradeService
-from src.schemas.trade import (
+from api.services.trade_service import TradeService
+from fastapi import APIRouter, Depends
+from fastapi_cache.decorator import cache
+from schemas.trade import (
     LastTradeResponse,
     TradeDynamicsRequest,
     TradeResponse,
     TradeResultsRequest,
 )
-from src.utils.unit_of_work import UnitOfWork
+from utils.unit_of_work import UnitOfWork
 
 router = APIRouter(prefix='/trades')
 
@@ -16,7 +18,21 @@ async def get_service(uow: UnitOfWork = Depends(UnitOfWork)) -> TradeService:
     return TradeService(uow)
 
 
+def cache_time():
+    """Возвращает значение для сброса кэша в 14:11.
+
+    :return: TTL (time-to-live) в секундах.
+    """
+    now = datetime.now()
+    next_reset = now.replace(hour=14, minute=11, second=0, microsecond=0)
+    if now >= next_reset:
+        next_reset += timedelta(days=1)
+    ttl = (next_reset - now).total_seconds()
+    return int(ttl)
+
+
 @router.get('/last_dates')
+@cache(expire=cache_time())
 async def get_last_trading_dates(
     limit: int,
     service: TradeService = Depends(get_service),
@@ -25,6 +41,7 @@ async def get_last_trading_dates(
 
 
 @router.get('/dynamics')
+@cache(expire=cache_time())
 async def get_dynamics(
     trade_filters: TradeDynamicsRequest,
     service: TradeService = Depends(get_service),
@@ -33,6 +50,7 @@ async def get_dynamics(
 
 
 @router.get('/last_results')
+@cache(expire=cache_time())
 async def get_trading_results(
     trade_filters: TradeResultsRequest,
     service: TradeService = Depends(get_service),
