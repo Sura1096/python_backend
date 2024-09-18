e703df7e5c2d3a82651eb04b04f45c36ffa34f77
Sura <suri.murodova@gmail.com>
2024-09-18 15:09:09 +0300

    Add redis to cache endpoints

--- FastAPI/src/main.py
+++ FastAPI/src/main.py
@@ -1,12 +1,21 @@
-import uvicorn
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
 
 from src.api.routers.trade import router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncIterator[None]:
+    redis = aioredis.from_url('redis://localhost')
+    FastAPICache.init(RedisBackend(redis), prefix='fastapi-cache')
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router)
-
-
-if __name__ == '__main__':
-    uvicorn.run(app='main:app', reload=True)
