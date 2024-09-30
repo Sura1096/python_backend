ce4e02c96d7d7c19566131f4d4f3200c7d113458
Sura <suri.murodova@gmail.com>
2024-09-30 20:24:34 +0300

    Add conftest

---
+++ FastAPI/tests/unit/conftest.py
@@ -0,0 +1,34 @@
+from collections.abc import Sequence
+from copy import deepcopy
+from typing import Any
+
+import pytest
+import pytest_asyncio
+from sqlalchemy import delete, select
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.models.spimex_model import SpimexTradingResults
+
+from tests import fixtures
+from tests.utils import save_object
+
+
+@pytest_asyncio.fixture
+async def setup_trade_info(
+    transaction_session: AsyncSession,
+    trade: dict[str, Any],
+) -> None:
+    await save_object(transaction_session, SpimexTradingResults, trade)
+
+
+@pytest_asyncio.fixture
+async def get_trades(transaction_session: AsyncSession):
+    async def _get_trades() -> Sequence[SpimexTradingResults]:
+        res = await transaction_session.execute(select(SpimexTradingResults))
+        return res.scalars().all()
+
+    return _get_trades
+
+
+@pytest.fixture
+def trade() -> dict[str, Any]:
+    return deepcopy(fixtures.postgres.TRADES)
