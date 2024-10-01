05dc2b6560fdc9631f1913b079ed53355ff04c6f
Sura <suri.murodova@gmail.com>
2024-10-01 20:20:12 +0300

    Fix global conftest

--- FastAPI/tests/conftest.py
+++ FastAPI/tests/conftest.py
@@ -3,14 +3,17 @@ from collections.abc import AsyncGenerator, Generator
 
 import pytest
 import pytest_asyncio
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 from httpx import AsyncClient
+from redis import asyncio as aioredis
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
     AsyncSession,
     create_async_engine,
 )
 from src.api.services.trade_service import TradeService
-from src.core.config import test_db
+from src.core.config import settings
 from src.main import app
 from src.models.base import Base
 from src.models.spimex_model import SpimexTradingResults
@@ -27,13 +30,14 @@ def event_loop(request: pytest.FixtureRequest) -> asyncio.AbstractEventLoop:
 
 @pytest_asyncio.fixture(scope='session')
 async def db_engine() -> AsyncGenerator[AsyncEngine, None]:
-    engine = create_async_engine(test_db.test_db_url())
+    engine = create_async_engine(settings.DB_URL)
     yield engine
     await engine.dispose()
 
 
 @pytest_asyncio.fixture(scope='session', autouse=True)
 async def setup_db(db_engine: AsyncEngine) -> None:
+    assert settings.MODE == 'TEST'
     async with db_engine.begin() as db_conn:
         await db_conn.run_sync(Base.metadata.drop_all)
         await db_conn.run_sync(Base.metadata.create_all)
@@ -43,13 +47,14 @@ async def setup_db(db_engine: AsyncEngine) -> None:
 async def transaction_session(
     db_engine: AsyncEngine,
 ) -> AsyncGenerator[AsyncSession, None]:
+    print('1')
     connection = await db_engine.connect()
     await connection.begin()
     session = AsyncSession(bind=connection)
 
     yield session
 
-    await session.rollback()
+    await connection.commit()  # Сохраняет данные в тестовую БД
     await connection.close()
 
 
@@ -57,8 +62,8 @@ async def transaction_session(
 def fake_trade_service(
     transaction_session: AsyncSession,
 ) -> Generator[FakeTradeService, None]:
-    _fake_trade_service = FakeTradeService(transaction_session)
-    yield _fake_trade_service
+    fake_trade_service = FakeTradeService(transaction_session)
+    yield fake_trade_service
 
 
 @pytest_asyncio.fixture
@@ -68,3 +73,9 @@ async def async_client(
     app.dependency_overrides[TradeService] = lambda: fake_trade_service
     async with AsyncClient(app=app, base_url='http://test') as ac:
         yield ac
+
+
+@pytest.fixture(scope='session', autouse=True)
+def init_cache() -> None:
+    redis = aioredis.from_url('redis://localhost')
+    FastAPICache.init(RedisBackend(redis), prefix='fastapi-cache')
