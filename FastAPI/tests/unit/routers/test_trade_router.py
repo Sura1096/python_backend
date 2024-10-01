0043fe1c1aea3919bc817272f341f3da25d7cd71
Sura <suri.murodova@gmail.com>
2024-10-01 20:27:10 +0300

    Add test class for trade router

--- FastAPI/tests/unit/routers/test_trade_router.py
+++ FastAPI/tests/unit/routers/test_trade_router.py
@@ -1,1 +1,20 @@
+import pytest
+from httpx import AsyncClient
 
+
+class TestTradeRouter:
+    @staticmethod
+    @pytest.mark.usefixtures('setup_trade_info')
+    async def test_get_last_trading_dates(
+        async_client: AsyncClient,
+    ) -> None:
+        params = {
+            'limit': 1,
+            'offset': 0,
+        }
+        response = await async_client.get('/trades/last_dates', params=params)
+        print(response.json())
+
+        assert response.status_code == 200
+        # assert len(response.json()['data']) == 1
+        # assert response.json()['data'][0]['trade_date'] == '2023-12-26'
