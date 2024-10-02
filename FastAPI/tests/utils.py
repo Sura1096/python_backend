5fd6337e3d4ccb81645c031e17eada4a1f934ce3
Sura <suri.murodova@gmail.com>
2024-10-02 17:08:46 +0300

    Add helper functions to check responses

--- FastAPI/tests/utils.py
+++ FastAPI/tests/utils.py
@@ -1,4 +1,5 @@
 from collections.abc import Sequence
+from datetime import date
 from typing import Any
 
 from pydantic import BaseModel
@@ -33,6 +34,42 @@ def compare_dicts_and_db_models(
         exp_item['id'] = res_item.id
 
     result_to_schema = [schema(**item.__dict__) for item in result]
+    expected_result_to_schema = [schema(**item) for item in expected_result]
+
+    equality_len = len(result_to_schema) == len(expected_result_to_schema)
+    equality_obj = all(obj in expected_result_to_schema for obj in result_to_schema)
+    return all((equality_len, equality_obj))
+
+
+def check_response_get_last_trading_dates(
+    result: list[dict] | None,
+    expected_result: list[dict] | None,
+) -> bool:
+    if result is None or expected_result is None:
+        return result == expected_result
+
+    result[0]['date'] = date.fromisoformat(result[0]['date'])
+
+    equality_len = len(result) == len(expected_result)
+    equality_obj = result[0]['date'] == expected_result[0]['date']
+    return all((equality_len, equality_obj))
+
+
+def check_response_get_dynamics(
+    result: list[dict] | None,
+    expected_result: list[dict] | None,
+    schema: type[BaseModel],
+) -> bool:
+    if result is None or expected_result is None:
+        return result == expected_result
+
+    for res_item, exp_item in zip(result, expected_result, strict=False):
+        exp_item['id'] = res_item['id']
+        res_item['date'] = date.fromisoformat(res_item['date'])
+        res_item['created_on'] = date.fromisoformat(res_item['created_on'])
+        res_item['updated_on'] = date.fromisoformat(res_item['updated_on'])
+
+    result_to_schema = [schema(**item) for item in result]
     expected_result_to_schema = [schema(**item) for item in expected_result]
 
     equality_len = len(result_to_schema) == len(expected_result_to_schema)
