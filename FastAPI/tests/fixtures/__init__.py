b90e462893f7a2a243cd9a4dddbe7be8cae9f9a6
Sura <suri.murodova@gmail.com>
2024-10-02 13:15:04 +0300

    Remove fake UoW and fake trade service

--- FastAPI/tests/fixtures/__init__.py
+++ FastAPI/tests/fixtures/__init__.py
@@ -1,45 +1,5 @@
 __all__ = [
-    'FakeBaseService',
-    'FakeTradeService',
-    'FakeUnitOfWork',
     'postgres',
 ]
 
-
-from types import TracebackType
-
-from sqlalchemy.ext.asyncio import AsyncSession
-from src.api.services.trade_service import TradeService
-from src.repositories.trade_repository import TradeRepository
-from src.utils.service import BaseService
-from src.utils.unit_of_work import UnitOfWork
-
 from tests.fixtures import postgres
-
-
-class FakeUnitOfWork(UnitOfWork):
-    def __init__(self, session: AsyncSession) -> None:
-        super().__init__()
-        self._session = session
-
-    async def __aenter__(self):
-        self.trade = TradeRepository(self._session)
-        return self
-
-    async def __aexit__(
-        self,
-        exc_type: type[BaseException] | None,
-        exc_val: BaseException | None,
-        exc_tb: TracebackType | None,
-    ) -> None:
-        await self._session.flush()
-
-
-class FakeBaseService(BaseService):
-    def __init__(self, session: AsyncSession) -> None:
-        super().__init__(UnitOfWork())
-        self.uow = FakeUnitOfWork(session)
-
-
-class FakeTradeService(FakeBaseService, TradeService):
-    pass
